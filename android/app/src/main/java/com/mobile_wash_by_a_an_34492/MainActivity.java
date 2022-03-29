3b31ed6c9f49b611c26cf6de15f67636627be2b8
Crowdbotics <team@crowdbotics.com>
2022-03-29 16:20:21 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/mobile_wash_by_a_an_34492/MainActivity.java
@@ -0,0 +1,15 @@
+package com.mobile_wash_by_a_an_34492;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "mobile_wash_by_a_an_34492";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "mobile_wash_by_a_an_34492",
+  url: "https://mobile_wash_by_a_an_34492.botics.co",
+  api: "https://mobile_wash_by_a_an_34492.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
